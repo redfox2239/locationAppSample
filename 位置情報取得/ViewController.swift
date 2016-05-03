f77be972376ade7de273e0d2d307bb94cd604f9f
HARADA REO <rharada@golyat.local>
2016-05-03 21:23:56 +0900

    Initial Commit

---
+++ 位置情報取得/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  位置情報取得
+//
+//  Created by HARADA REO on 2016/05/03.
+//  Copyright © 2016年 reo harada. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
